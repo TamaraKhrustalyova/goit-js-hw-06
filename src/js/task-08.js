f99b02032d5f39522dfe61966df74b30e8712295
TamaraKhrustalyova <tamara.khrustalyova@gmail.com>
2023-05-11 09:05:38 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const formRef = document.querySelector('.login-form');
+console.log(formRef.elements);
+
+const {email, password} = formRef.elements;
+
+formRef.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+    event.preventDefault();
+    const formData = {
+        email: email.value,
+        password: password.value,
+    }
+    console.log(formData);
+  
+    if(email.value.trim() === "" || password.value.trim() === "") {
+        alert("Please fill in all the form fields");
+    } else {
+        alert("Data sent");
+        formRef.reset();
+    }
+}
 
