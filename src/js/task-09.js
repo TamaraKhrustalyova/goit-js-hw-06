6aff8d7a3e9245249d39824daf1b009a8a4afcf4
TamaraKhrustalyova <tamara.khrustalyova@gmail.com>
2023-05-11 10:42:42 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,25 @@
+
+  const bodyEl = document.querySelector('body');
+  const buttonEl = document.querySelector('.change-color');
+  const colorEl = document.querySelector('.color');
+
+  console.log(bodyEl);
+  console.log(buttonEl);
+
+buttonEl.addEventListener('click', changeBackgroundColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function changeBackgroundColor() {
+  getRandomHexColor();
+  let randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  colorEl.textContent = randomColor;
+}
+
+
+
